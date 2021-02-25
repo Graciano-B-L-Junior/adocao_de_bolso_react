cec86192ee4e6f000dd4b94d4d44e38015506109
Graciano Junior <gracianobarbosalimajunior@gmail.com>
2021-02-25 15:32:50 -0300

    Criando a primeira tela da aplicacao

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <View>
+        <Text style={{ textAlign: 'center' }}>Logo</Text>
+        <View style={{alignItems:'center'}}>
+          <View style={{ width: '30%', paddingTop: 50 }}>
+            <Button title="Login" style={styles.button}></Button>
+          </View>
+
+          <TouchableOpacity style={{paddingTop:50}}>
+            <Text style={{borderBottomWidth:1,borderBottomColor:'black'}}>Esqueci minha senha</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={{paddingTop:50}}>
+            <Text style={{borderBottomWidth:1,borderBottomColor:'black'}}>Novo usuário</Text>
+          </TouchableOpacity>
+        </View>
+
+
+      </View>
     </View>
   );
 }
@@ -14,8 +31,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gray',
     justifyContent: 'center',
   },
+  button: {
+
+  }
 });

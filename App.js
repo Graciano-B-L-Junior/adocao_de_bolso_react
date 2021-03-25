473697206dec30b261c4a7907311db2f5c560208
Graciano Junior <gracianobarbosalimajunior@gmail.com>
2021-03-25 13:17:17 -0300

    alterando o design das telas para finalizacao do produto

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet,Image, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import Cadastro from './Cadastro';
 import Contato from './Contato';
 import Trilha from './Trilha';
@@ -44,30 +45,32 @@ export default function App() {
     if (tela == 'inicio') {
       return (
         <View style={styles.container}>
+          <LinearGradient colors={['rgb(247,139,13)','rgb(64,129,156)']} start={{x:0,y:0.2}} style={{height:'100%', paddingTop:50}}>
           <StatusBar hidden />
           <View style={{ alignItems: 'center' }}>
-            <View style={{ justifyContent: 'center', width: 150, height: 150, borderWidth: 1 }}>
-              <Text style={{ textAlign: 'center' }}>Logo</Text>
+            <View style={{ justifyContent: 'center', width: 200, height: 200 }}>
+              <Image source={require('./assets/newIcons/logoAdB.png')} style={{resizeMode:'stretch',width:'100%',height:'100%'}}></Image>
             </View>
 
-            <View style={{ alignItems: 'center', paddingTop: 50 }}>
-              <TextInput value="login" style={{ width: 250, height: 40, borderWidth: 1, marginTop: 15, paddingLeft: 5, borderRadius: 25 }}></TextInput>
-              <TextInput value="senha" style={{ width: 250, height: 40, borderWidth: 1, marginTop: 15, paddingLeft: 5, borderRadius: 25 }}></TextInput>
+            <View style={{ alignItems: 'center', paddingTop: 20 }}>
+              <TextInput value="usuario" style={{ width: 290, height: 40, borderWidth: 1, marginTop: 15, paddingLeft: 5, borderRadius: 25,borderColor:'#f7f7f7',color:'#f7f7f7',textAlign:'center' }}></TextInput>
+              <TextInput value="senha" style={{ width: 290, height: 40, borderWidth: 1, marginTop: 15, paddingLeft: 5, borderRadius: 25,borderColor:'#f7f7f7',color:'#f7f7f7',textAlign:'center' }}></TextInput>
               <View style={{ width: 100, paddingTop: 25 }}>
-                <TouchableOpacity style={{ width: '100%', backgroundColor: 'black', height: 30, borderRadius: 25 }} onPress={() => setTela('Trilha')}>
+                <TouchableOpacity style={{ width: '100%', backgroundColor: 'rgb(247,139,13)', height: 30, borderRadius: 25 }} onPress={() => setTela('Trilha')}>
                   <Text style={{ textAlign: 'center', fontSize: 20, color: 'white' }}>Login</Text>
                 </TouchableOpacity>
               </View>
 
-              <TouchableOpacity style={{ paddingTop: 50 }} onPress={()=>setTela('NovaSenha')}>
-                <Text style={{ borderBottomWidth: 1, borderBottomColor: 'black' }}>Esqueci minha senha</Text>
+              <TouchableOpacity style={{ paddingTop: 90 }} onPress={()=>setTela('NovaSenha')}>
+                <Text style={{ borderBottomWidth: 1, borderBottomColor: '#f7f7f7', color:'#f7f7f7' }}>Esqueci a senha</Text>
               </TouchableOpacity>
 
               <TouchableOpacity onPress={() => setTela('cadastro')} style={{ paddingTop: 10 }}>
-                <Text style={{ borderBottomWidth: 1, borderBottomColor: 'black' }}>Novo usuário</Text>
+                <Text style={{ borderBottomWidth: 1, borderBottomColor: '#f7f7f7', color:'#f7f7f7' }}>Criar uma conta</Text>
               </TouchableOpacity>
             </View>
           </View>
+          </LinearGradient>
         </View>
       );
     }
--- Trilha.js
+++ Trilha.js
@@ -1,78 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Trilha(props) {
 
     return (
         <View style={styles.container}>
-            <StatusBar hidden></StatusBar>
-            <View style={styles.headerTela}>
-                <TouchableOpacity style={styles.btnInfo} onPress={() => props.setTela('Contato')}>
-                    <Image style={styles.iconStyle} source={require('./assets/icones-20.png')}>
-                    </Image>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.btnPerfil} onPress={() => props.setTela('Perfilusuario')}>
-                    <Image style={styles.iconStyle} source={require('./assets/icones-21.png')}>
-                    </Image>
-                </TouchableOpacity>
-                <Text style={{ fontSize: 20, fontFamily: 'Inter_500Medium' }}>Adoção de bolso</Text>
-            </View>
-            <ScrollView>
-                <View style={{ paddingTop: 100, justifyContent: 'center', alignItems: 'center' }}>
-                    <View>
-                        <TouchableOpacity style={styles.btnBalaoTrilhaL}>
-                            <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp1.png')} ></Image>
-                        </TouchableOpacity>
-                        <Text style={{ right: 50, paddingTop: 10 }}>Passo 1</Text>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.btnBalaoTrilhaR}>
-                            <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp2.png')} ></Image>
-                        </TouchableOpacity>
-                        <Text style={{ left: 125, paddingTop: 10 }}>Passo 2</Text>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.btnBalaoTrilhaL}>
-                            <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp3.png')} ></Image>
-                        </TouchableOpacity>
-                        <Text style={{ right: 50, paddingTop: 10 }}>Passo 3</Text>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.btnBalaoTrilhaR}>
-                            <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp4.png')} ></Image>
-                        </TouchableOpacity>
-                        <Text style={{ left: 125, paddingTop: 10 }}>Passo 4</Text>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.btnBalaoTrilhaL}>
-                            <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp5.png')} ></Image>
-                        </TouchableOpacity>
-                        <Text style={{ right: 50, paddingTop: 10 }}>Passo 5</Text>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.btnBalaoTrilhaR}>
-                            <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp6.png')} ></Image>
-                        </TouchableOpacity>
-                        <Text style={{ left: 125, paddingTop: 10 }}>Passo 6</Text>
-                    </View>
-                    <View style={{ paddingBottom: 100 }}></View>
+            <LinearGradient colors={['rgb(255,85,0)', 'rgb(247,139,13)']} start={{ x: 0, y:0 }} end={{ x: 0, y: 0.99 }}>
+                <StatusBar hidden></StatusBar>
+                <View style={styles.headerTela}>
+                    <TouchableOpacity style={styles.btnInfo} onPress={() => props.setTela('Contato')}>
+                        <Image style={styles.iconStyle} source={require('./assets/newIcons/info.png')}>
+                        </Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnPerfil} onPress={() => props.setTela('Perfilusuario')}>
+                        <Image style={styles.iconStyle} source={require('./assets/icones-21.png')}>
+                        </Image>
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 20, fontFamily: 'Inter_500Medium', color: 'white' }}><Text style={{ fontFamily: 'Inter_700Bold', color: 'white' }}>Adoção</Text> de bolso</Text>
                 </View>
-            </ScrollView>
-            <View style={styles.bottomNav}>
-                <TouchableOpacity style={styles.btnInfo} onPress={() => props.setTela('DicasCulturais')}>
-                    <Image style={styles.iconStyle} source={require('./assets/icones-18.png')}>
-                    </Image>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.btnTrilha}>
-                    <Image style={styles.iconStyleTrilha} source={require('./assets/icones-17.png')}>
-                    </Image>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.btnPerfil} onPress={() => props.setTela('PerguntasFrequentes')}>
-                    <Image style={styles.iconStyle} source={require('./assets/icones-16.png')}>
-                    </Image>
-                </TouchableOpacity>
-            </View>
+                <ScrollView>
+                    <View style={{ paddingTop: 100, justifyContent: 'center', alignItems: 'center' }}>
+                        <View>
+                            <TouchableOpacity style={styles.btnBalaoTrilhaL}>
+                                <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp1.png')} ></Image>
+                            </TouchableOpacity>
+                            <Text style={{ right: 50, paddingTop: 10, color:'white',fontFamily:'Inter_700Bold' }}>Passo 1</Text>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={styles.btnBalaoTrilhaR}>
+                                <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp2.png')} ></Image>
+                            </TouchableOpacity>
+                            <Text style={{ left: 125, paddingTop: 10, color:'white',fontFamily:'Inter_700Bold' }}>Passo 2</Text>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={styles.btnBalaoTrilhaL}>
+                                <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp3.png')} ></Image>
+                            </TouchableOpacity>
+                            <Text style={{ right: 50, paddingTop: 10, color:'white',fontFamily:'Inter_700Bold' }}>Passo 3</Text>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={styles.btnBalaoTrilhaR}>
+                                <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp4.png')} ></Image>
+                            </TouchableOpacity>
+                            <Text style={{ left: 125, paddingTop: 10, color:'white',fontFamily:'Inter_700Bold' }}>Passo 4</Text>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={styles.btnBalaoTrilhaL}>
+                                <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp5.png')} ></Image>
+                            </TouchableOpacity>
+                            <Text style={{ right: 50, paddingTop: 10, color:'white',fontFamily:'Inter_700Bold' }}>Passo 5</Text>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={styles.btnBalaoTrilhaR}>
+                                <Image style={styles.iconEtapaTrilha} source={require('./assets/newIcons/trilha/etp6.png')} ></Image>
+                            </TouchableOpacity>
+                            <Text style={{ left: 125, paddingTop: 10, color:'white',fontFamily:'Inter_700Bold' }}>Passo 6</Text>
+                        </View>
+                        <View style={{ paddingBottom: 100 }}></View>
+                    </View>
+                </ScrollView>
+                <View style={styles.bottomNav}>
+                    <TouchableOpacity style={styles.btnInfo} onPress={() => props.setTela('DicasCulturais')}>
+                        <Image style={styles.iconStyle} source={require('./assets/newIcons/dicasOff.png')}>
+                        </Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnTrilha}>
+                        <Image style={styles.iconStyleTrilha} source={require('./assets/newIcons/trilhaOn.png')}>
+                        </Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnPerfil} onPress={() => props.setTela('PerguntasFrequentes')}>
+                        <Image style={styles.iconStyle} source={require('./assets/newIcons/duvidasOff.png')}>
+                        </Image>
+                    </TouchableOpacity>
+                </View>
+            </LinearGradient>
         </View>
 
     )
@@ -81,17 +84,17 @@ export default function Trilha(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'rgb(255,85,0)'
     },
     headerTela: {
         width: '100%',
         height: 50,
-        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
         position: 'absolute',
         top: 0,
-        zIndex: 99
+        zIndex: 99,
+        backgroundColor: 'rgb(255,85,0)'
     },
     inputTextStyle: {
         width: '80%',
@@ -109,42 +112,58 @@ const styles = StyleSheet.create({
     bottomNav: {
         width: '100%',
         height: 70,
-        backgroundColor: 'white',
+        backgroundColor: '#f7f7f7',
         alignItems: 'center',
         justifyContent: 'center',
         position: 'absolute',
         bottom: 0,
-        zIndex: 99,
-        borderTopWidth: 1
+        zIndex: 99
     },
     btnTrilha: {
         borderWidth: 2,
         borderRadius: 50,
-        backgroundColor: 'white',
+        backgroundColor: '#f78b0d',
         width: 90,
         height: 90,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        bottom: 20,
+        borderWidth: 4,
+        borderColor: '#f7f7f7'
     },
     btnBalaoTrilhaL: {
         height: 120,
         width: 120,
-        borderWidth: 2,
+        borderWidth: 4,
+        borderColor: 'white',
         borderRadius: 100,
         right: 90,
         backgroundColor: '#f78b0d',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        shadowColor: '#000',
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 1,
+
     },
     btnBalaoTrilhaR: {
         height: 120,
         width: 120,
-        borderWidth: 2,
+        borderWidth: 4,
+        borderColor: 'white',
         borderRadius: 100,
         left: 90,
         backgroundColor: '#f78b0d',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 1,
     },
     iconStyle: {
         width: 42,
